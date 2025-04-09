53cb5dccff9419ab5bc722572d15cd6ab42f3871
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-04-09 15:08:17 +0200

    Création vue nouveau dans views.py

--- elevage/lapin/views.py
+++ elevage/lapin/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from forms import ElevageForm
 
-# Create your views here.
+def nouveau(request):
+    if request.method == 'POST':
+        form = ElevageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('liste_elevages')  # Redirection à définir plus tard
+    else:
+        form = ElevageForm()
+    return render(request, 'elevage/nouveau.html', {'form': form})
