5a29f2d505e140a3aa936f37222aa9b4e7748244
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-04-09 20:40:38 +0200

    Redirection vers liste elevage apres nouvel elevage

--- elevage/lapin/views.py
+++ elevage/lapin/views.py
@@ -7,7 +7,7 @@ def nouveau(request):
         form = ElevageForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('nouveau_elevage')  # Redirection à définir plus tard
+            return redirect('liste_elevages')  # Redirection à définir plus tard
     else:
         form = ElevageForm()
     return render(request, 'elevage/nouveau.html', {'form': form})
