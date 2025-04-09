a0820a67588d2581fd620030aef589d7e0a19f6a
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-04-09 16:54:43 +0200

    création vue liste qui renvoie tous les elevages par ordre de creation

--- elevage/lapin/views.py
+++ elevage/lapin/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import ElevageForm
+from .models import Elevage
 
 def nouveau(request):
     if request.method == 'POST':
@@ -10,3 +11,7 @@ def nouveau(request):
     else:
         form = ElevageForm()
     return render(request, 'elevage/nouveau.html', {'form': form})
+
+def liste(request):
+    elevages = Elevage.objects.all().order_by('-date_creation')
+    return render(request, 'elevage/liste.html', {'elevages': elevages})
