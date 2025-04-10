970548f69390a9b795d0d85c940d038d2225a178
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-04-10 11:48:52 +0200

    Ajout class Regle au modele pour définir les regles du jeu

--- elevage/lapin/models.py
+++ elevage/lapin/models.py
@@ -33,6 +33,30 @@ class Individu(models.Model):
     def __str__(self):
         return f"{self.get_sexe_display()} - {self.age} mois - {self.get_etat_display()}"
 
-    
+from django.db import models
+
+class Regle(models.Model):
+    # Prix
+    prix_nourriture = models.DecimalField(max_digits=5, decimal_places=3, help_text="Prix au g de nourriture")
+    prix_cage = models.DecimalField(max_digits=5, decimal_places=2)
+    prix_vente_lapin = models.DecimalField(max_digits=5, decimal_places=2)
+
+    # Consommation
+    conso_2_mois = models.DecimalField(max_digits=4, decimal_places=2, help_text="Conso en g/mois pour un lapin de 2 mois")
+    conso_3_mois_et_plus = models.DecimalField(max_digits=4, decimal_places=2, help_text="Conso en g/mois à partir de 3 mois")
+
+    # Reproduction
+    age_min_gravide = models.IntegerField(help_text="Âge minimum (mois) pour être gravide")
+    age_max_gravide = models.IntegerField(help_text="Âge maximum (mois) pour être gravide")
+    duree_gestation = models.IntegerField(help_text="Durée en mois")
+    nb_max_par_portee = models.IntegerField(help_text="Nombre max de lapereaux par portée")
+
+    # Cages
+    nb_max_individus_par_cage = models.IntegerField(help_text="Nombre normal d'individus par cage")
+    seuil_surpopulation = models.IntegerField(help_text="Seuil à partir duquel il y a surpopulation")
+
+    def __str__(self):
+        return "Règle de jeu"
+
 
 
