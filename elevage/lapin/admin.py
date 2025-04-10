c694616783237905f18a595ca38ea69cbb821d60
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-04-10 12:05:59 +0200

    Ajout regles au portail admin pour les définir

--- elevage/lapin/admin.py
+++ elevage/lapin/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Elevage, Individu
+from .models import Elevage, Individu, Regle
 
 @admin.register(Elevage)
 class ElevageAdmin(admin.ModelAdmin):
@@ -11,3 +11,23 @@ class ElevageAdmin(admin.ModelAdmin):
 class IndividuAdmin(admin.ModelAdmin):
     list_display = ('elevage', 'sexe', 'age', 'etat')
     list_filter = ('elevage', 'sexe', 'etat')
+    
+@admin.register(Regle)
+class RegleAdmin(admin.ModelAdmin):
+    list_display = (
+        'prix_nourriture',
+        'prix_cage',
+        'prix_vente_lapin',
+        'conso_2_mois',
+        'conso_3_mois_et_plus',
+        'age_min_gravide',
+        'age_max_gravide',
+        'duree_gestation',
+        'nb_max_par_portee',
+        'nb_max_individus_par_cage',
+        'seuil_surpopulation',
+    )
+
+    search_fields = ('prix_nourriture', 'prix_cage', 'prix_vente_lapin')
+    
+
