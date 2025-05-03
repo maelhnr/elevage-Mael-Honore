a7b62195db47916b8bb57c1648506eaaa94f8e19
Maël Honoré <mael.honore@student.isae-supaero.fr>
2025-05-03 02:32:23 +0200

    Ajout Modele client pour user, avatar et premium avec création automatique d'un user

--- elevage/lapin/models.py
+++ elevage/lapin/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from random import randint, choice
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Elevage(models.Model):
@@ -162,5 +165,21 @@ class Regle(models.Model):
     def __str__(self):
         return "Règle de jeu"
 
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
+    is_premium = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.user.username
+
+# Création automatique du profil Client à la création d'un User
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Client.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.client.save()
 
